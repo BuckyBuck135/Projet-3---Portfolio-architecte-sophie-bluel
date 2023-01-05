df50c7b111ec82687412040b0cfc474b3759a9bb
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-01-05 12:32:43 +0100

    Refactored fetch and render functions

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -6,192 +6,75 @@ const objectsWorksBtn = document.getElementById("objects-works")
 const apartmentsWorksBtn = document.getElementById("apartments-works")
 const hotelsWorksBtn = document.getElementById("hotels-works")
 
-//Fetch all Works and renders gallery portfolio//
-function fetchGallery() {
-    fetch("http://localhost:5678/api/works")
-    .then(function(res) {
-        if (res.ok) {
-          return res.json();
-        }
-    })
-    .then(function(value) {
-        //renders the works
-        for (let i=0; i<value.length; i++) {
-            const figure = document.createElement("figure")
-            figure.innerHTML = `
-                <img src="${value[i].imageUrl}" alt="${value[i].title}" crossorigin="anonymous">
-                <figcaption>${value[i].title}</figcaption>
-            `
-            galleryEl.appendChild(figure)
-        }
-    })
-    .catch(function(err) {
-        // Une erreur est survenue
-    });
+//////////////////// HOME PAGE PORTFOLIO ////////////////////
+
+// Render all works by default
+renderAllWorks()
+
+// Fetch Works on http://localhost:5678/api/works
+async function fetchApi() {
+    try {
+        const data = await fetch("http://localhost:5678/api/works");
+        return data.json();
+    } 
+    catch (error) {
+        return error;
+    }
 }
 
-//Fetch Objects Works and renders them//
-
-function fetchObjectsWorks() {
-    fetch("http://localhost:5678/api/works")
-    .then(function(res) {
-        if (res.ok) {
-          return res.json();
-        }
-    })
-    .then(function(value) {
-        //renders the works
-        for (let i=0; i<value.length; i++) {
-            //checks category property for OBJECTS (categoryId = 1)
-            if(value[i].categoryId === 1) {
-                const figure = document.createElement("figure")
-                figure.innerHTML = `
-                <img src="${value[i].imageUrl}" alt="${value[i].title}" crossorigin="anonymous">
-                <figcaption>${value[i].title}</figcaption>
-                `
-                galleryEl.appendChild(figure)
-            } 
-        }
-    })
-    .catch(function(err) {
-        // Une erreur est survenue
-    });
+// Render all Works
+async function renderAllWorks() {
+    const works = await fetchApi();
+    for (let i=0; i<works.length; i++) {
+        const figure = document.createElement("figure")
+        figure.innerHTML = `
+            <img src="${works[i].imageUrl}" alt="${works[i].title}" crossorigin="anonymous">
+            <figcaption>${works[i].title}</figcaption>
+        `
+        galleryEl.appendChild(figure)
+    }
 }
 
-//Fetch Apartments Works and renders them//
-
-function fetchApartmentsWorks() {
-    fetch("http://localhost:5678/api/works")
-    .then(function(res) {
-        if (res.ok) {
-          return res.json();
-        }
-    })
-    .then(function(value) {
-        //renders the works
-        for (let i=0; i<value.length; i++) {
-            //checks category property for APARTMENTS (categoryId = 2)
-            if(value[i].categoryId === 2) {
+// Render filetered Works
+async function renderFilteredWorks(category) {
+    const works = await fetchApi();
+    for (let i=0; i<works.length; i++) {
+        //checks category property for HOTELS (categoryId = 3)
+            if(works[i].categoryId === category) {
                 const figure = document.createElement("figure")
                 figure.innerHTML = `
-                <img src="${value[i].imageUrl}" alt="${value[i].title}" crossorigin="anonymous">
-                <figcaption>${value[i].title}</figcaption>
+                    <img src="${works[i].imageUrl}" alt="${works[i].title}" crossorigin="anonymous">
+                    <figcaption>${works[i].title}</figcaption>
                 `
                 galleryEl.appendChild(figure)
-            } 
-        }
-    })
-    .catch(function(err) {
-        // Une erreur est survenue
-    });
-}
-
-function fetchHotelsWorks() {
-    fetch("http://localhost:5678/api/works")
-    .then(function(res) {
-        if (res.ok) {
-          return res.json();
-        }
-    })
-    .then(function(value) {
-        //renders the works
-        for (let i=0; i<value.length; i++) {
-            //checks category property for HOTELS (categoryId = 3)
-            if(value[i].categoryId === 3) {
-                const figure = document.createElement("figure")
-                figure.innerHTML = `
-                <img src="${value[i].imageUrl}" alt="${value[i].title}" crossorigin="anonymous">
-                <figcaption>${value[i].title}</figcaption>
-                `
-                galleryEl.appendChild(figure)
-            } 
-        }
-    })
-    .catch(function(err) {
-        // Une erreur est survenue
-    });
+            }
+    }
 }
 
 // Event listeners for Filter buttons
 allWorksBtn.addEventListener("click", function() {
     galleryEl.innerHTML = ""
-    fetchGallery()
+    renderAllWorks()
 })
 
 objectsWorksBtn.addEventListener("click", function() {
     galleryEl.innerHTML = ""
-    fetchObjectsWorks()
+    renderFilteredWorks(1)
 })
 
 apartmentsWorksBtn.addEventListener("click", function() {
     galleryEl.innerHTML = ""
-    fetchApartmentsWorks()
+    renderFilteredWorks(2)
 })
 
 hotelsWorksBtn.addEventListener("click", function() {
     galleryEl.innerHTML = ""
-    fetchHotelsWorks()
+    renderFilteredWorks(3)
 })
 
-// Renders all works on load
-// fetchGallery()
 
-///////////////////////////////////////////
-// TRYING TO REFACTOR THE FETCH FUNCTION //
-///////////////////////////////////////////
+//////////////////// END OF HOME PAGE PORTFOLIO ////////////////////
 
-// let worksObject;
-// function fetchWorks() {
-//     fetch("http://localhost:5678/api/works")
-//     .then(function(res) {
-//         if (res.ok) {
-//             let worksObject = res.json();
-//             return worksObject;
-//         }
-//     })
-// }
-// console.log(fetchWorks())
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-// // api url
-// const api_url = 
-//       "http://localhost:5678/api/works";
-  
-// // Defining async function
-// async function getapi(url) {
-    
-//     // Storing response
-//     const response = await fetch(url);
-    
-//     // Storing data in form of JSON
-//     let works = await response.json();
-//     console.log(works);
-//     // if (response) {
-//     //     hideloader();
-//     // }
-//     renderPortfolio(works);
-// }
-// // Calling that async function
-// getapi(api_url);
-
-
-
-
-
-// function renderPortfolio(data) {
-//     for (let i=0; i<data.length; i++) {
-//         const figure = document.createElement("figure")
-//         figure.innerHTML = `
-//             <img src="${data[i].imageUrl}" alt="${data[i].title}">
-//             <figcaption>${data[i].title}</figcaption>
-//         `
-//         galleryEl.appendChild(figure)
-// }}
-// renderPortfolio()
-
-//////////////////////////////////////////////////////////////////////////////////////////
 
 // // Fetch categories and function to filter works
 // function fetchCategories() {
