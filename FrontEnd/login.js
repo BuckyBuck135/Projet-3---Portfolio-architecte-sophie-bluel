6c9b948401f8ed2f94e20affa1fc63015b260103
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-01-11 19:03:29 +0800

    Testing submit button

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,7 +1,33 @@
 const loginForm = document.getElementById("login-form")
+const emailInput = document.getElementById("email-input")
+const passwordInput = document.getElementById("password-input")
+
+
+
 // let isLoggedIn = true;
 
+function sendLogin(e) {
+    e.preventDefault();
+    loginForm.style.color = "red"
+
+    // fetch("http://localhost:5678/api/users/login", {
+    //     method: "POST",
+    //     headers: {
+    //       'Accept': 'application/json', 
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         email: emailInput.value,
+    //         password: passwordInput.value
+    //     })
+    //   })
+    //   .then(function(res) {
+    //     if (res.ok) {
+    //         window.redirect("/index.html")  
+    //       return res.json();
+    //     }
+    //   })
+    }
+
 // Submits login form //
-contactForm.addEventListener("submit", function(e) {
-    e.preventDefault()
-})
+loginForm.addEventListener("submit", sendLogin)
